aa2269a1ba526374c49ecff7588a916285541dbb
Yesid Rivera <ripyesid33@gmail.com>
2025-10-02 13:20:21 -0500

    Version 0.3: Converted the Stateless widgets that we needed to change into Stateful ones

--- poke_api/lib/screens/main_screen.dart
+++ poke_api/lib/screens/main_screen.dart
@@ -1,4 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; // Needed for JSON decoding
+// At the top of the file where the error occurs (e.g., your main widget file)
+
+// Define the Post class (your data model)
+class Pokemon {
+  final String name;
+  final String abilities;
+
+  Pokemon({required this.name, required this.abilities});
+
+  // Factory method to create a Post object from a JSON map
+  factory Pokemon.fromJson(Map<String, dynamic> json) {
+    return Pokemon(name: json['name'], abilities: json['abilities']);
+  }
+}
+
+Future<Pokemon> fetchPost() async {
+  final uri = Uri.parse(
+    'https://pokeapi.co/api/v2/pokemon/ditto1',
+  ); // Replace with your API endpoint
+  final response = await http.get(uri);
+
+  if (response.statusCode == 200) {
+    // If the server returns an OK response, parse the JSON.
+    final jsonBody = json.decode(response.body);
+    return Pokemon.fromJson(jsonBody);
+  } else {
+    // If the server did not return a 200 OK response,
+    // throw an exception.
+    throw Exception('Failed to load post. Status code: ${response.statusCode}');
+  }
+}
 
 class FullHomeScreen extends StatefulWidget {
   const FullHomeScreen({super.key});
@@ -10,16 +43,25 @@ class FullHomeScreen extends StatefulWidget {
 class _FullHomeScreenState extends State<FullHomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(),
-      body: Column(
-        children: [
-          SizedBox(height: 150),
-          PokemonImage(),
-          SizedBox(height: 80),
-          PokemonDescription(),
-        ],
-      ),
+    return Scaffold(appBar: CustomAppBar(), body: HomeScreenBody());
+  }
+}
+
+class HomeScreenBody extends StatelessWidget {
+  const HomeScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 150),
+        PokemonImage(
+          url:
+              "https://images.wikidexcdn.net/mwuploads/wikidex/7/77/latest/20150621181250/Pikachu.png",
+        ),
+        SizedBox(height: 80),
+        PokemonDescription(name: "Pikachu", type: "Electric", habitat: "Por ahi"),
+      ],
     );
   }
 }
@@ -30,7 +72,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      title: Text("Pokemon Name"),
+      title: Text("Pokemon Getter"),
       backgroundColor: Colors.blue,
       centerTitle: true,
       leading: IconButton(onPressed: () {}, icon: const Icon(Icons.catching_pokemon_outlined)),
@@ -41,9 +83,22 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
-class PokemonDescription extends StatelessWidget {
-  const PokemonDescription({super.key});
+class PokemonDescription extends StatefulWidget {
+  final String name;
+  final String type;
+  final String habitat;
+  const PokemonDescription({
+    super.key,
+    required this.name,
+    required this.type,
+    required this.habitat,
+  });
 
+  @override
+  State<PokemonDescription> createState() => _PokemonDescriptionState();
+}
+
+class _PokemonDescriptionState extends State<PokemonDescription> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -55,27 +110,28 @@ class PokemonDescription extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          children: [Text("Pokemon type"), Text("Pokemon powers"), Text("Pokemon Habitat")],
+          children: [
+            Text("Pokemon name: ${widget.name}"),
+            Text("Pokemon type: ${widget.type}"),
+            Text("Pokemon Habitat: ${widget.habitat}"),
+          ],
         ),
       ),
     );
   }
 }
 
-class PokemonImage extends StatelessWidget {
-  const PokemonImage({super.key});
+class PokemonImage extends StatefulWidget {
+  final String url;
+  const PokemonImage({super.key, required this.url});
 
   @override
+  State<PokemonImage> createState() => _PokemonImageState();
+}
+
+class _PokemonImageState extends State<PokemonImage> {
+  @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        SizedBox(width: 40),
-        Image.network(
-          "https://images.wikidexcdn.net/mwuploads/wikidex/7/77/latest/20150621181250/Pikachu.png",
-          height: 200,
-          width: 300,
-        ),
-      ],
-    );
+    return Row(children: [SizedBox(width: 40), Image.network(widget.url, height: 200, width: 300)]);
   }
 }
